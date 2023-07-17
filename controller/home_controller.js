f89d2214a98d979b19b521927eba72abbcf986a0
geekpiyush <dpiyush28@gmail.com>
2023-07-17 10:21:59 +0530

    successfully show posts on home page

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,9 +1,16 @@
-module.exports.home = function(req,res)
-{
-    console.log(req.cookies);
-    res.cookie('user_id',1010)
-    return res.render('home',
-    {
-        title:'Codial | Home'
+const Post = require('../model/post');
+
+module.exports.home = function (req, res) {
+  Post.find({})
+    .populate('user')
+    .then((posts) => {
+      return res.render('home', {
+        title: 'Codial | Home',
+        posts: posts,
+      });
     })
-}
+    .catch((err) => {
+      console.log('An error occurred:', err);
+      return res.status(500).json({ error: 'An error occurred' });
+    });
+};
